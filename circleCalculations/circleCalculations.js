29b76378d1f53f714c1385dbfe87816fe5d30b2a
Hummingbird <>
2019-09-11 10:01:07 -0700

    This is my first commit

--- circleCalculations/circleCalculations.js
+++ circleCalculations/circleCalculations.js
@@ -4,7 +4,24 @@
 
 //Bonus 2: Make both numbers whole integers (aka round to the nearest integer) 
 
-function circleCalculation () {
-	//your code here
+// Area and perimeter of circle with radius given
+// input:radius:a number
+// var calculated area
+// var calculated perimeter
+// Formula area=pi*r^2
+// Formula perimeter=2*pi*r
+// return statement that is the string with the variables
+// output: area and perimeter
 
+var area;
+var perimeter;
+function circleCalculation (radius) {
+	//your code here
+	area=Math.PI * radius * radius ;
+	perimeter= Math.PI * 2 * radius;
+	return "The area of the circle is " + area + " and the perimeter of the circle is " + perimeter;
+}
 circleCalculation(10);
+console.log(perimeter);
+console.log(area);
+console.log(circleCalculation);
